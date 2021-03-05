01d0c24cacfde108f1171daa18d4a9c610218232
kalipso <kalipso@c3d2.de>
2021-03-05 15:16:23 +0100

    use boost::program_options

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+
+#include <boost/program_options.hpp>
 #include "spdlog/spdlog.h"
 
-int main()
+namespace po = boost::program_options;
+
+int main(int argc, char **argv)
 {
-	spdlog::info("Hello World!");
+	po::options_description description("mdump - multicast filesharing for the commandline");
+
+	const auto print_help = [&description](){ std::cout << description; };
+
+	description.add_options()
+		("help,h", "Display help message");
+
+	po::variables_map vm;
+	po::store(po::command_line_parser(argc, argv).options(description).run(), vm);
+	po::notify(vm);
+
+	if(vm.count("help"))
+	{
+		print_help();
+		return 0;
+	}
+
 	return 0;
 }
+
