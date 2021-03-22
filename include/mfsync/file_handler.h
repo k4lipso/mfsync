63aa6e4c2cb57e0b1676e1f7594c03d8749e4141
kalipso <kalipso@c3d2.de>
2021-03-22 20:33:24 +0100

    add ofstream_wrapper

--- include/mfsync/file_handler.h
+++ include/mfsync/file_handler.h
@@ -7,6 +7,7 @@
 #include <optional>
 #include <condition_variable>
 #include <filesystem>
+#include <fstream>
 
 namespace mfsync
 {
@@ -73,13 +74,83 @@ namespace mfsync
     return lhs.file_info < rhs.file_info;
   }
 
+	class ofstream_wrapper
+	{
+	public:
+		ofstream_wrapper(const requested_file& file)
+			: requested_file_(file)
+		{
+		}
+
+		~ofstream_wrapper()
+		{
+			if(auto shared_token = write_token_.lock())
+			{
+				*shared_token.get() = false;
+			}
+
+			ofstream_.close();
+		}
+
+		ofstream_wrapper(ofstream_wrapper&& other) = default;
+		ofstream_wrapper& operator=(ofstream_wrapper&& other) = default;
+
+		bool operator!() const
+		{
+			return !ofstream_;
+		}
+
+		void open(const std::string& filename, std::ios_base::openmode mode = std::ios_base::out)
+		{
+			ofstream_.open(filename.c_str(), mode);
+		}
+
+		void open(const char *filename, std::ios_base::openmode mode = std::ios_base::out)
+		{
+			ofstream_.open(filename, mode);
+		}
+
+		void write(const char* s, std::streamsize count, size_t offset = 0)
+		{
+			ofstream_.seekp(offset);
+			ofstream_.write(s, count);
+			ofstream_.flush();
+		}
+
+		void write(const char* chunk)
+		{
+			write(chunk, requested_file_.chunksize, 0);
+		}
+
+		std::ofstream::traits_type::pos_type tellp()
+		{
+			return ofstream_.tellp();
+		}
+
+		void flush()
+		{
+			ofstream_.flush();
+		}
+
+		void set_token(std::weak_ptr<std::atomic<bool>> token)
+		{
+			write_token_ = token;
+		}
+
+		std::ofstream ofstream_;
+
+	private:
+
+		mfsync::requested_file requested_file_;
+		std::weak_ptr<std::atomic<bool>> write_token_;
+	};
 
   class file_handler
   {
   public:
     using stored_files = std::set<file_information, std::less<>>;
     using available_files = std::set<available_file, std::less<>>;
-    using locked_files = std::vector<std::pair<std::string, std::shared_ptr<std::atomic<bool>>>>;
+    using locked_files = std::vector<std::pair<file_information, std::shared_ptr<std::atomic<bool>>>>;
     file_handler() = default;
     ~file_handler() = default;
 
