e1a5b88a6d8904d603269619d2f832b38f96b127
kalipso <kalipso@c3d2.de>
2021-03-23 11:58:03 +0100

    mv file_sender impl's into src file

--- include/mfsync/file_sender.h
+++ include/mfsync/file_sender.h
@@ -18,75 +18,13 @@ namespace mfsync::multicast
   {
   public:
     file_sender(boost::asio::io_service& io_service,
-        const boost::asio::ip::address& multicast_address,
-        short multicast_port,
-        file_handler* filehandler)
-      : endpoint_(multicast_address, multicast_port)
-      , socket_(io_service, endpoint_.protocol())
-      , timer_(io_service)
-      , file_handler_(filehandler)
+                const boost::asio::ip::address& multicast_address,
+                short multicast_port,
+                file_handler* filehandler);
 
-    {
-      init();
-    }
-
-    void init()
-    {
-      auto messages = protocol::create_messages_from_file_info(file_handler_->get_stored_files());
-
-      if(messages.empty())
-      {
-        timer_.expires_from_now(boost::posix_time::seconds(1));
-        timer_.async_wait(std::bind(&file_sender::init, this));
-        return;
-      }
-
-      for(const auto& message : messages)
-      {
-        socket_.async_send_to(
-            boost::asio::buffer(message), endpoint_,
-            std::bind(&file_sender::handle_send_to, this,
-              std::placeholders::_1));
-      }
-
-    }
-
-    void handle_send_to(const boost::system::error_code& error)
-    {
-      if(!error)
-      {
-        timer_.expires_from_now(boost::posix_time::seconds(1));
-        timer_.async_wait( std::bind(&file_sender::handle_timeout, this,
-              std::placeholders::_1));
-      }
-    }
-
-    void handle_timeout(const boost::system::error_code& error)
-    {
-      if (!error)
-      {
-        auto messages = protocol::create_messages_from_file_info(file_handler_->get_stored_files());
-
-        if(!messages.empty())
-        {
-          message_ = messages.front();
-        }
-
-        for(const auto& message : messages)
-        {
-          if(message.empty())
-          {
-            continue;
-          }
-
-          spdlog::debug("Sending Message: '{}'", message);
-          socket_.async_send_to(
-              boost::asio::buffer(message), endpoint_,
-              std::bind(&file_sender::handle_send_to, this,
-                std::placeholders::_1));
-        }
-      }
-    }
+    void init();
+    void handle_send_to(const boost::system::error_code& error);
+    void handle_timeout(const boost::system::error_code& error);
 
   private:
     boost::asio::ip::udp::endpoint endpoint_;
