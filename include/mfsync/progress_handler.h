191fc1e5aad6369925e67d572dde168f4186a237
kalipso <kalipso@c3d2.de>
2022-07-13 16:12:16 +0200

    mv progress_handler impls into src file

--- include/mfsync/progress_handler.h
+++ include/mfsync/progress_handler.h
@@ -35,16 +35,7 @@ namespace progress
       : file_information{info}
     {}
 
-    bool update_bar()
-    {
-      if(bar == nullptr)
-      {
-        return false;
-      }
-
-      bool result = update_status();
-      return result || update_progress();
-    }
+    bool update_bar();
 
     std::atomic<size_t> bytes_transferred = 0;
     std::atomic<STATUS> status = STATUS::UNKNOWN;
@@ -53,83 +44,8 @@ namespace progress
 
   private:
 
-    bool update_status()
-    {
-      if(status == old_status)
-      {
-        return false;
-      }
-
-      old_status.store(status.load());
-
-      std::string message;
-      auto color = indicators::Color::unspecified;
-      switch(old_status)
-      {
-        case STATUS::UPLOADING:
-          {
-            color = Color::green;
-            message = "uploading: ";
-            break;
-          }
-        case STATUS::DOWNLOADING:
-          {
-            color = Color::red;
-            message = "downloading: ";
-            break;
-          }
-        case STATUS::COMPARING:
-          {
-            color = Color::blue;
-            message = "comparing: ";
-            break;
-          }
-        case STATUS::DONE:
-          {
-            color = Color::yellow;
-            message = "done: ";
-            break;
-          }
-        case STATUS::INITIALIZING:
-          {
-            color = Color::cyan;
-            message = "initializing: ";
-            break;
-          }
-        case STATUS::UNKNOWN:
-          {
-            color = Color::white;
-            message = "unknown: ";
-            break;
-          }
-      }
-
-      auto hash_name = sha256sum;
-      hash_name.resize(8);
-      bar->set_option(option::PrefixText(message + hash_name));
-      bar->set_option(option::ForegroundColor(color));
-
-      return true;
-    }
-
-    bool update_progress()
-    {
-      if(bytes_transferred == old_bytes_transferred)
-      {
-        return false;
-      }
-
-      old_bytes_transferred.store(bytes_transferred.load());
-      const size_t percentage = (static_cast<double>(old_bytes_transferred) / size) * 100;
-      bar->set_progress(percentage);
-
-      if(percentage == 100 && !bar->is_completed())
-      {
-        bar->mark_as_completed();
-      }
-
-      return true;
-    }
+    bool update_status();
+    bool update_progress();
 
     std::atomic<STATUS> old_status = STATUS::UNKNOWN;
     std::atomic<size_t> old_bytes_transferred = 0;
@@ -149,45 +65,14 @@ public:
   void start();
   void stop();
 
-  file_progress_ptr create_file_progress(const file_information& file_info)
-  {
-    const auto compare_func = [&file_info](const auto& info)
-    {
-      return file_info.file_name == info->file_name
-          && file_info.sha256sum == info->sha256sum;
-    };
-
-    std::unique_lock lk{mutex_};
-    const auto find_result = std::find_if(files_.begin(), files_.end(), compare_func);
-
-    if(find_result != files_.end())
-    {
-      return find_result->get();
-    }
-
-    auto file_progress = std::make_unique<progress::file_progress_information>(file_info);
-    file_progress->bar = create_bar();
-
-    auto* result = file_progress.get();
-    files_.push_back(std::move(file_progress));
-    return result;
-  }
+  file_progress_ptr create_file_progress(const file_information& file_info);
 
 private:
 
-  progress::bar_ptr create_bar()
-  {
-    auto bar = std::make_unique<ProgressBar>(option::BarWidth{50},
-                                             option::ForegroundColor{Color::red},
-                                             option::ShowPercentage{true}
-                                             /*option::PrefixText{filename}*/);
-    bars_.push_back(*bar.get());
-    return bar;
-  }
+  progress::bar_ptr create_bar();
 
   std::mutex mutex_;
   progress_vector files_;
-
   DynamicProgress<ProgressBar> bars_;
   std::thread worker_thread_;
   bool running_ = true;
--- src/progress_handler.cpp
+++ src/progress_handler.cpp
@@ -3,6 +3,99 @@
 namespace mfsync::filetransfer
 {
 
+namespace progress
+{
+
+  bool file_progress_information::update_bar()
+  {
+    if(bar == nullptr)
+    {
+      return false;
+    }
+
+    bool result = update_status();
+    return result || update_progress();
+  }
+
+  bool file_progress_information::update_status()
+  {
+    if(status == old_status)
+    {
+      return false;
+    }
+
+    old_status.store(status.load());
+
+    std::string message;
+    auto color = indicators::Color::unspecified;
+    switch(old_status)
+    {
+      case STATUS::UPLOADING:
+        {
+          color = Color::green;
+          message = "uploading: ";
+          break;
+        }
+      case STATUS::DOWNLOADING:
+        {
+          color = Color::red;
+          message = "downloading: ";
+          break;
+        }
+      case STATUS::COMPARING:
+        {
+          color = Color::blue;
+          message = "comparing: ";
+          break;
+        }
+      case STATUS::DONE:
+        {
+          color = Color::yellow;
+          message = "done: ";
+          break;
+        }
+      case STATUS::INITIALIZING:
+        {
+          color = Color::cyan;
+          message = "initializing: ";
+          break;
+        }
+      case STATUS::UNKNOWN:
+        {
+          color = Color::white;
+          message = "unknown: ";
+          break;
+        }
+    }
+
+    auto hash_name = sha256sum;
+    hash_name.resize(8);
+    bar->set_option(option::PrefixText(message + hash_name));
+    bar->set_option(option::ForegroundColor(color));
+
+    return true;
+  }
+
+  bool file_progress_information::update_progress()
+  {
+    if(bytes_transferred == old_bytes_transferred)
+    {
+      return false;
+    }
+
+    old_bytes_transferred.store(bytes_transferred.load());
+    const size_t percentage = (static_cast<double>(old_bytes_transferred) / size) * 100;
+    bar->set_progress(percentage);
+
+    if(percentage == 100 && !bar->is_completed())
+    {
+      bar->mark_as_completed();
+    }
+
+    return true;
+  }
+}
+
 progress_handler::progress_handler()
 {
   show_console_cursor(false);
@@ -42,5 +135,40 @@ void progress_handler::stop()
 {
   running_ = false;
 }
+
+progress_handler::file_progress_ptr progress_handler::create_file_progress(const file_information& file_info)
+{
+  const auto compare_func = [&file_info](const auto& info)
+  {
+    return file_info.file_name == info->file_name
+        && file_info.sha256sum == info->sha256sum;
+  };
+
+  std::unique_lock lk{mutex_};
+  const auto find_result = std::find_if(files_.begin(), files_.end(), compare_func);
+
+  if(find_result != files_.end())
+  {
+    return find_result->get();
+  }
+
+  auto file_progress = std::make_unique<progress::file_progress_information>(file_info);
+  file_progress->bar = create_bar();
+
+  auto* result = file_progress.get();
+  files_.push_back(std::move(file_progress));
+  return result;
+}
+
+progress::bar_ptr progress_handler::create_bar()
+{
+  auto bar = std::make_unique<ProgressBar>(option::BarWidth{50},
+                                           option::ForegroundColor{Color::red},
+                                           option::ShowPercentage{true}
+                                           /*option::PrefixText{filename}*/);
+  bars_.push_back(*bar.get());
+  return bar;
+}
+
 
 } //closing namespace mfsync::filetransfer
