8c400b96228a01184dad27bb7c862af1667ff748
kalipso <kalipso@c3d2.de>
2022-08-31 11:31:12 +0200

    add example test that uses libmfsync

--- test/unit/test.cpp
+++ test/unit/test.cpp
@@ -2,8 +2,16 @@
 
 #include <catch2/catch.hpp>
 
+#include "mfsync/file_handler.h"
+
 unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "mfsync example test", "[example_test]" ) {
+  auto handler = mfsync::file_handler();
+  const auto stored = handler.get_stored_files();
+  REQUIRE(stored.empty());
 }
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
