0f58254f638663779c361e743d4514351c35efd4
kalipso <kalipso@c3d2.de>
2023-03-22 21:25:15 +0100

    add --list-hosts option that prints key - ip:port - version of sharing hosts

--- include/mfsync/file_fetcher.h
+++ include/mfsync/file_fetcher.h
@@ -23,14 +23,40 @@ public:
                mfsync::crypto::crypto_handler& crypto_handler);
 
   void handle_receive_from(const boost::system::error_code& error, size_t bytes_recvd);
+  void list_hosts(bool value) { list_host_infos_ = value; }
 
 private:
+  void print_host_if_new(const host_information& host_info)
+  {
+    if(host_infos_.contains(host_info.public_key))
+    {
+      return;
+    }
+
+    spdlog::info("{} - {}:{} - v{}", host_info.public_key,
+                                     host_info.ip,
+                                     host_info.port,
+                                     host_info.version);
+
+    host_infos_.emplace(host_info.public_key);
+  }
+
+  void do_receive()
+  {
+    socket_.async_receive_from(boost::asio::buffer(data_, max_length), sender_endpoint_,
+                               boost::bind(&file_fetcher::handle_receive_from, this,
+                                           boost::asio::placeholders::error,
+                                           boost::asio::placeholders::bytes_transferred));
+  }
+
   mutable std::mutex mutex_;
   boost::asio::io_context& io_context_;
   boost::asio::ip::udp::socket socket_;
   mfsync::file_handler* file_handler_;
   mfsync::crypto::crypto_handler& crypto_handler_;
   boost::asio::ip::udp::endpoint sender_endpoint_;
+  bool list_host_infos_ = false;
+  std::set<std::string> host_infos_;
   enum { max_length = 1024 };
   char data_[max_length];
 };
--- src/file_fetcher.cpp
+++ src/file_fetcher.cpp
@@ -37,42 +37,47 @@ namespace mfsync::multicast
   void file_fetcher::handle_receive_from(const boost::system::error_code& error, size_t bytes_recvd)
   {
     std::scoped_lock lk{mutex_};
-    if (!error)
-    {
-      spdlog::trace("Received Message: '{}'", std::string(data_, bytes_recvd));
-      spdlog::trace("From: {}", boost::lexical_cast<std::string>(sender_endpoint_.address()));
-
-
-      auto host_info = mfsync::protocol::get_host_info_from_message(std::string(data_, bytes_recvd),
-                                                                    sender_endpoint_);
-
-      if(host_info.has_value())
-      {
-        spdlog::debug("received host info, ip: {}, port: {}, pubkey: {}",
-                      host_info.value().ip,
-                      host_info.value().port,
-                      host_info.value().public_key);
-      }
-
-      if(crypto_handler_.trust_key(host_info.value().public_key))
-      {
-        const auto session =
-            std::make_shared<mfsync::filetransfer::client_encrypted_file_list>(
-              io_context_,
-              *file_handler_,
-              crypto_handler_,
-              host_info.value());
-        session->start_request();
-      }
-
-      socket_.async_receive_from(boost::asio::buffer(data_, max_length), sender_endpoint_,
-                                 boost::bind(&file_fetcher::handle_receive_from, this,
-                                             boost::asio::placeholders::error,
-                                             boost::asio::placeholders::bytes_transferred));
-    }
-    else
+    if (error)
     {
       spdlog::error("Error in handle_receive_from: {}", error.message());
+      return;
     }
+
+    spdlog::trace("Received Message: '{}'", std::string(data_, bytes_recvd));
+    spdlog::trace("From: {}", boost::lexical_cast<std::string>(sender_endpoint_.address()));
+
+    auto host_info = mfsync::protocol::get_host_info_from_message(std::string(data_, bytes_recvd),
+                                                                  sender_endpoint_);
+
+    if(!host_info.has_value())
+    {
+      do_receive();
+      return;
+    }
+
+    spdlog::debug("received host info, ip: {}, port: {}, pubkey: {}",
+                  host_info.value().ip,
+                  host_info.value().port,
+                  host_info.value().public_key);
+
+    if(list_host_infos_)
+    {
+      print_host_if_new(host_info.value());
+      do_receive();
+      return;
+    }
+
+    if(crypto_handler_.trust_key(host_info.value().public_key))
+    {
+      const auto session =
+          std::make_shared<mfsync::filetransfer::client_encrypted_file_list>(
+            io_context_,
+            *file_handler_,
+            crypto_handler_,
+            host_info.value());
+      session->start_request();
+    }
+
+    do_receive();
   }
 }
