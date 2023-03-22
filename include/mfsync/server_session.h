679a59ffa98f42bd6e000ff5e89841481cc42713
kalipso <kalipso@c3d2.de>
2023-03-22 12:57:21 +0100

    clangformart serversession.h

--- include/mfsync/server_session.h
+++ include/mfsync/server_session.h
@@ -1,82 +1,80 @@
 #pragma once
 
-#include <utility>
 #include <boost/asio.hpp>
 #include <boost/asio/ssl.hpp>
+#include <utility>
 
-#include "mfsync/file_handler.h"
 #include "mfsync/client_session.h"
 #include "mfsync/crypto.h"
+#include "mfsync/file_handler.h"
 
-namespace mfsync::filetransfer
-{
+namespace mfsync::filetransfer {
 
 using SSLSocket = boost::asio::ssl::stream<boost::asio::ip::tcp::socket>;
 
-template<typename SocketType>
-class server_session_base : public std::enable_shared_from_this<server_session_base<SocketType>>
-{
-public:
-  server_session_base() = delete;
-  server_session_base(SocketType socket,
-                      mfsync::file_handler& handler,
-                      mfsync::crypto::crypto_handler& crypto_handler);
-  virtual ~server_session_base() = default;
+template <typename SocketType>
+class server_session_base
+    : public std::enable_shared_from_this<server_session_base<SocketType>> {
+   public:
+    server_session_base() = delete;
+    server_session_base(SocketType socket, mfsync::file_handler& handler,
+                        mfsync::crypto::crypto_handler& crypto_handler);
+    virtual ~server_session_base() = default;
 
-  SocketType& get_socket();
-  virtual void start() = 0;
-  void read();
+    SocketType& get_socket();
+    virtual void start() = 0;
+    void read();
 
-  void set_progress(progress_handler* progress)
-  {
-    progress_ = progress;
-  }
+    void set_progress(progress_handler* progress) { progress_ = progress; }
 
+   protected:
+    void handle_read_header(boost::system::error_code const& error,
+                            std::size_t bytes_transferred);
+    void send_confirmation();
+    void respond_encrypted(const std::string& pub_key);
+    void reply_with_error(const std::string& reason);
+    void read_confirmation();
+    void handle_read_confirmation(boost::system::error_code const& error,
+                                  std::size_t bytes_transferred);
+    void write_file();
 
-protected:
-
-  void handle_read_header(boost::system::error_code const &error, std::size_t bytes_transferred);
-  void send_confirmation();
-  void respond_encrypted(const std::string& pub_key);
-  void reply_with_error(const std::string& reason);
-  void read_confirmation();
-  void handle_read_confirmation(boost::system::error_code const &error, std::size_t bytes_transferred);
-  void write_file();
-
-  SocketType socket_;
-  mfsync::file_handler& file_handler_;
-  mfsync::crypto::crypto_handler& crypto_handler_;
-  std::string message_;
-  std::string public_key_;
-  requested_file requested_;
-  boost::asio::streambuf stream_buffer_;
-  std::vector<unsigned char> writebuf_;
-  std::ifstream ifstream_;
-  progress_handler* progress_;
-  progress::file_progress_information* bar_ = nullptr;
+    SocketType socket_;
+    mfsync::file_handler& file_handler_;
+    mfsync::crypto::crypto_handler& crypto_handler_;
+    std::string message_;
+    std::string public_key_;
+    requested_file requested_;
+    boost::asio::streambuf stream_buffer_;
+    std::vector<unsigned char> writebuf_;
+    std::ifstream ifstream_;
+    progress_handler* progress_;
+    unsigned port_ = 0;
+    progress::file_progress_information* bar_ = nullptr;
 };
 
-class server_session : public server_session_base<boost::asio::ip::tcp::socket>
-{
-public:
-  server_session() = delete;
-  server_session(boost::asio::ip::tcp::socket socket, mfsync::file_handler& handler, mfsync::crypto::crypto_handler& crypto_handler);
-  virtual ~server_session() = default;
+class server_session
+    : public server_session_base<boost::asio::ip::tcp::socket> {
+   public:
+    server_session() = delete;
+    server_session(boost::asio::ip::tcp::socket socket,
+                   mfsync::file_handler& handler,
+                   mfsync::crypto::crypto_handler& crypto_handler);
+    virtual ~server_session() = default;
 
-  virtual void start() override;
+    virtual void start() override;
 };
 
-class server_tls_session : public server_session_base<SSLSocket>
-{
-public:
-  server_tls_session() = delete;
-  server_tls_session(SSLSocket socket, mfsync::file_handler& handler, mfsync::crypto::crypto_handler& crypto_handler);
-  virtual ~server_tls_session() = default;
+class server_tls_session : public server_session_base<SSLSocket> {
+   public:
+    server_tls_session() = delete;
+    server_tls_session(SSLSocket socket, mfsync::file_handler& handler,
+                       mfsync::crypto::crypto_handler& crypto_handler);
+    virtual ~server_tls_session() = default;
 
-  virtual void start() override;
+    virtual void start() override;
 
-private:
-  void do_handshake();
+   private:
+    void do_handshake();
 };
 
-} //closing namespace mfsync::filetransfer
+}  // namespace mfsync::filetransfer
